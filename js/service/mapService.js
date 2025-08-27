02aa58f3610390ab3413afae8cdd96527e22e129
pjs <inceal@iyouproject.com>
2025-08-27 16:10:21 +0900

    fix: 오타수정 .part of INO-241

--- js/service/mapService.js
+++ js/service/mapService.js
@@ -59,7 +59,7 @@ const MapService = (() => {
       } else {
         MarkerService.allElementShow();
       }
-    });햣
+    });
   };
   const mapClickEvent = () => {
     if (isClickEvent == false) {
