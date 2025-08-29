aaddf65738bb6f979a0f9e03cb28440168375e12
JiSeoung1997 <inceal@iyouproject.com>
2025-08-29 13:23:53 +0900

    셔틀트레인 ui 및 소요시간 탭 segment 연결선 수정 (#65)
    * fix: map을 zoomout했을 때 element가 사라지지 않는 현상 수정.part of INO-257
    
    * fix: polylineview가 zoomlevel이 20일때 viewPolyline이 작동하지 않는 현상 수정.part of INO-241
    
    * fix: 오타수정 .part of INO-241
    
    * fix: 오타수정 미적용 해결 .part of INO-241
    
    * fix: polylineEvent수정으로 인한 element show 미작동 문제 수정.part of INO-257
    
    * improvement: 바텀시트 및 마커 1출국장 추가 .part of INO-278
    
    * improvement: 1출국장 추가에 따른 bottomSheet 높이조절 .part of INO-278
    
    * improvement: bottomSheet 높이 변경으로 인한 locacon위치변경 .part of INO-278
    
    * improvement: locaCon 위치 조정 .part of INO-278
    
    * fix: mockData2 내용 수정으로 인한 오류 수정.part of INO-278
    
    * fix: 마커이동속도 수정, langcon 수정, bottomSheetUI수정.part of INO-284
    
    * fix: bottomSheet 높이 조절, errorPage root수정.part of INO-284
    
    * fix: bottomSheet 모바일최적화 높이 수정 .part of INO-284
    
    * fix:트레인바 언어변경 내용 수정 .part of INO-284
    
    * fix: main.js PolylineService중복 import 수정.part of INO-284
    
    * fix:셔틀트레인 ui 수정.part of INO-284

--- js/component/customControl.js
+++ js/component/customControl.js
@@ -88,7 +88,7 @@ const CustomControl = (() => {
     if(selectedLang =='ko'){
       marginBottom= 12.5
     }else{
-      marginBottom=14.5
+      marginBottom=17
     }
     const locationBtnHtml =
       '<div id="requestLocation" style="height:2rem;display:flex ;align-items: center;justify-content: center;background-color:#fff;border-radius:1rem 1rem 1rem 1rem ;width:2rem;margin-right:10px;margin-bottom:'+marginBottom+'rem"><img id = "gps-black"src="./images/gps_black.svg" style="height:25px; width:25px;" ><img id ="gps-blue" src="./images/gps_blue.svg" style="height:1.5rem; width:1.5rem;display:none;" ></div>';
