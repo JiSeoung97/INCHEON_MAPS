3428df605915f71e61c6a0f691794d0f5197ea11
JiSeoung1997 <inceal@iyouproject.com>
2025-09-02 10:20:40 +0900

    러닝 환경 체크 alert 삽입 (#80)
    * fix: map을 zoomout했을 때 element가 사라지지 않는 현상 수정.part of INO-257
    
    * fix: polylineview가 zoomlevel이 20일때 viewPolyline이 작동하지 않는 현상 수정.part of INO-241
    
    * fix: 오타수정 .part of INO-241
    
    * fix: 오타수정 미적용 해결 .part of INO-241
    
    * fix: polylineEvent수정으로 인한 element show 미작동 문제 수정.part of INO-257
    
    * improvement: 바텀시트 및 마커 1출국장 추가 .part of INO-278
    
    * improvement: 1출국장 추가에 따른 bottomSheet 높이조절 .part of INO-278
    
    * improvement: bottomSheet 높이 변경으로 인한 locacon위치변경 .part of INO-278
    
    * improvement: locaCon 위치 조정 .part of INO-278
    
    * fix: mockData2 내용 수정으로 인한 오류 수정.part of INO-278
    
    * fix: 마커이동속도 수정, langcon 수정, bottomSheetUI수정.part of INO-284
    
    * fix: bottomSheet 높이 조절, errorPage root수정.part of INO-284
    
    * fix: bottomSheet 모바일최적화 높이 수정 .part of INO-284
    
    * fix:트레인바 언어변경 내용 수정 .part of INO-284
    
    * fix: main.js PolylineService중복 import 수정.part of INO-284
    
    * fix:셔틀트레인 ui 수정.part of INO-284
    
    * fix:mapclick시출국장 테두리 초기화 .part of INO-278
    
    * fix:zoomChanged 이벤트 .part of INO-278
    
    * fix:segment line 수정 .part of INO-278
    
    * fix:eastWest div click시 language가 바뀌어도 info의language가 수정되지 않는 현상 수정 .part of INO-278
    
    * fix:다크모드 제한 .part of INO-278
    
    * fix: errorPage 이동시 css오류 수정 .part of INO-278
    
    * fix: 언어변경시 elementMarker사라지는 현상 수정.part of INO-278
    
    * fix: bottomSheet 예상 소요시간 탭 img png->svg로 수정.part of INO-278
    
    * fix: errorPage root 수정.part INO-278
    
    * fix: langauge 변경시 줌에 따라 element marker생성 .part of INO-278
    
    * fix: map ZoomLevel이 18단계 때도 element Marker가 생성되도록 수정.part of INO-278
    
    * fix: build.js파일에서 errorPage.html에 대한 처리 추가 .part of INO-278
    
    * fix: build.js html처리 수정.part of INO-278
    
    * fix: build.js html처리 재수정 .part of INO-278
    
    * fix: build시 css파일 분리해서 build .part of INO-278
    
    * fix: zoomLevel이 18보다 낮을 때 elementMarker hide 하도록 변경 .part of INO-278
    
    * fix:전역 errorHandler 주석해제.part of INO-278
    
    * improvement: utLocation에서 사용자 위치가져오기 수정 .part of INO-278
    
    * improvement: browser에 따른 옵션 함수 생성 .part of INO-278
    
    * fix: 언어변경시 drag최소치 변경 .part of INO-278
    
    * fix: desktop에서 현위치 요청시 watch기능 사용안하도록 수정 .part of INO-278
    
    * fix:언어설정이 영어 or 중국어 일시 이동구간 높이 증가.part of INO-278
    
    * fix: 처음 user Location 요청 위치 변경 .part of INO-278
    
    * fix: android 확인용 alert 삽입 .part of INO-278
    
    * test: 러닝 환경 체크 alert 삽입 .part of INO-278

--- js/utility/location.js
+++ js/utility/location.js
@@ -8,8 +8,8 @@ const utLocation = (() => {
   let locationCallbacks = [];
 
   let option;
-  let isIOS;
-  let isAndroid;
+  let isIOS = null;
+  let isAndroid = null;
   const getBrowserOptimizedOptions = () => {
     const userAgent = navigator.userAgent;
     isIOS = /iPad|iPhone|iPod/.test(userAgent);
@@ -55,6 +55,8 @@ const utLocation = (() => {
         maximumAge: 0,
       };
     }
+    alert(option.timeout);
+    console.log(option);
   };
   // GPS 예열 함수
   const warmUpGPS = () => {
@@ -205,6 +207,7 @@ const utLocation = (() => {
   return {
     init: () => {
       getBrowserOptimizedOptions(); // 사용자 브라우저 확인
+      setTimeout(() => {}, 1000);
       warmUpGPS(); // GPS 예열
       startWatching(); // 지속적 추적 시작
     },
