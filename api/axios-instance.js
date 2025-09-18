6a0931239967346344e28fe7dad13afc5a969287
YoonJongHo <llsee01@iyouproject.com>
2025-09-18 16:44:10 +0900

    fix: axios 인스턴스 헤더에 타입 수정

--- api/axios-instance.js
+++ api/axios-instance.js
@@ -3,7 +3,7 @@ function createAxiosInstance(config) {
     baseURL: "https://f7zndrait3.execute-api.ap-northeast-2.amazonaws.com/dev", // 새로 생성된 API Gateway 주소
     timeout: 5000,
     headers: {
-      type: "json",
+      
     },
   });
   console.log("instance생성완료");
