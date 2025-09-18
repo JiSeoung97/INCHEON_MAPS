0bb8893ac3a093b9e879f541e003015b9cd3c55f
YoonJongHo <llsee01@iyouproject.com>
2025-09-18 18:01:32 +0900

    fix: amplify 환경변수 path 수정

--- amplify/backend/function/congestionProxy/src/app.js
+++ amplify/backend/function/congestionProxy/src/app.js
@@ -1,18 +1,4 @@
 /*
-Use the following code to retrieve configured secrets from SSM:
-
-const aws = require('aws-sdk');
-
-const { Parameters } = await (new aws.SSM())
-  .getParameters({
-    Names: ["AIR_API_KEY"].map(secretName => process.env[secretName]),
-    WithDecryption: true,
-  })
-  .promise();
-
-Parameters will be of the form { Name: 'secretName', Value: 'secretValue', ... }[]
-*/
-/*
 Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
     http://aws.amazon.com/apache2.0/
@@ -38,10 +24,10 @@ app.use(function(req, res, next) {
 });
 
 // 인천공항 API를 위한 프록시 경로입니다.
-app.get('/airport/getDepartureCongestion', async (req, res) => {
+app.get('/api/airport/getDepartureCongestion', async (req, res) => {
   try {
     // API_BASE_URL과 AIR_API_KEY는 나중에 람다 함수 환경변수로 설정할 것입니다.
-    const targetUrl = `${process.env.API_BASE_URL}${req.path}`;
+    const targetUrl = process.env.API_BASE_URL;
 
     const response = await axios.get(targetUrl, {
       params: { ...req.query, serviceKey: process.env.AIR_API_KEY },
