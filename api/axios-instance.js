5d28758e62c0d292c2405b0a74db32db04bfcf7e
JiSeoung1997 <inceal@iyouproject.com>
2025-09-29 15:37:35 +0900

    api Test (#114)
    * improvement: locaCon 위치 bottomSheet드래그시 변경하도록 수정 .part of INO-308
    
    * styleSheet 누락으로인한 재 커밋
    
    * fix:bottomSheet에 locaCon이 종속됨으로 drag최소 높이 수정.part of INO-309
    
    * fix: 초기 로딩시 줌레벨, map center좌표 수정 .part of INO-311
    
    * feature: mapService init시 쿼리스트링으로 받은 언어 세션스토리지에 저장 .part of INO-312
    
    * fix: 혼잡도 데이터 핸들링중 동서편이 바뀌어들어가는 오류 수정 .part of INO-313
    
    * improvement:혼잡도페이지 스켈레톤ui추가
    
    * improvement:혼잡도 로딩속도 개선.part of INO-314
    
    * improvement: 스켈레톤 ui적용 시간 축소 및 jquery slim버전으로 수정 .part of INO-314
    
    * fix:대표님 요청으로 인한 push
    
    * fix: 첫로딩시 지도 중앙 위치 수정 .part of INO-315
    
    * Fix: drag open 높이 수정 및 현재 위치 수정시마다 recoArray 재설정 .part of INO-316
    
    * 빈 커밋
    
    * improvement: 교통센터 gif추가
    
    * Fix: drag 로직변경 및 불필요 로그 삭제 .part of INO-343
    
    * Fix: 실제교통센터 키오스크에 맞춰 gif파일 수정 .part of INO-344
    
    * improvement: 추천로직 추가 기존 추천로직에서 출국장 pass까지의 시간이 같으면 대기자 수를 비교하여 추천 .part of INO-345
    
    * fix: drag로직 변경 rem => dvh .part of INO-343
    
    * fix: drag 로직변경
    
    * api호출 확인
    
    ---------
    
    Co-authored-by: YoonJongHo <llsee01@iyouproject.com>

--- api/axios-instance.js
+++ api/axios-instance.js
@@ -1,6 +1,6 @@
 function createAxiosInstance(config) {
   const instance = axios.create({
-    baseURL: "https://f7zndrait3.execute-api.ap-northeast-2.amazonaws.com/dev", // 새로 생성된 API Gateway 주소
+    baseURL: "https://wiki.urcode.link/api/v1/airport/congestion/departure", // 새로 생성된 API Gateway 주소
     timeout: 5000,
     headers: {},
   });
