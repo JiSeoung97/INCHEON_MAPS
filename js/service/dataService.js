7a72e7b07cb9f6440e88d390d43057dd525a1748
pjs <inceal@iyouproject.com>
2025-09-30 14:20:19 +0900

    fix: apiData데이터 접근로직 수정.part of INO-344

--- js/service/dataService.js
+++ js/service/dataService.js
@@ -53,7 +53,7 @@ const DataService = (() => {
       const result = await window.axios.get(apiurl, {
         params: requestParams,
       });
-      apiDatas = result?.data?.parsed_data?.body?.items?.item || null;
+      apiDatas = result?.data?.data?.parsed_data?.body?.items?.item || null;
       Logger.log("실시간 혼잡도 API 수신 완료", apiDatas);
     } catch (error) {
       apiDatas = mockData2?.data?.parsed_data?.body?.items?.item;
